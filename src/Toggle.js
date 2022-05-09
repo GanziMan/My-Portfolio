ba121e9df182d732e1c07e1dcc9d3d711150feb5
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2022-05-09 19:49:52 +0900

    2022/05/09

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { func, string } from 'prop-types';
-import styled from 'styled-components';
+
 // Import a couple of SVG files we'll use in the design: https://www.flaticon.com
 import './Toggle.styled.css';
 
